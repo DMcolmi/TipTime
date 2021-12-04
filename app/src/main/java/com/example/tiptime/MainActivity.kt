b1eff76c5f66c7133747eed7d96301b1c6b42326
davide <davidemaria.colmi@gmail.com>
2021-12-04 21:16:16 +0100

    migration to material design - step n.1

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -14,7 +14,7 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+        calculateTip()
         binding.calculateButton.setOnClickListener { calculateTip() }
     }
 
