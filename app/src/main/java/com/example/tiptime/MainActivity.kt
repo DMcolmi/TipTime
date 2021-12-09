e9d99444bddc102d5c26d926e17c3cc8142a92d2
davide <davidemaria.colmi@gmail.com>
2021-12-09 16:24:38 +0100

    updated icons

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.example.tiptime
 
+import android.content.Context
 import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import androidx.appcompat.app.AppCompatActivity
 import com.example.tiptime.databinding.ActivityMainBinding
 import java.text.NumberFormat
@@ -16,6 +20,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
         calculateTip()
         binding.calculateButton.setOnClickListener { calculateTip() }
+         binding.costOfService.setOnKeyListener { view, keyCode, _ ->  handleKeyEvent(view, keyCode) }
     }
 
     private fun calculateTip() {
@@ -43,6 +48,15 @@ class MainActivity : AppCompatActivity() {
     private fun setTipResult(tip : Double){
         val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
         binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
+    }
+
+    private fun handleKeyEvent(view: View, keyCode: Int) :Boolean{
+        if(keyCode == KeyEvent.KEYCODE_ENTER){
+            val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken,0)
+            return true
+        }
+        return false
     }
 
 }
